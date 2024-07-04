40c71121c190d47607a9bfcb858bddbde2cbf797
ritwickrajmakhal <ritwickrajmakhal11@gmail.com>
2024-07-05 01:22:30 +0530

    cors changed

--- config/middlewares.js
+++ config/middlewares.js
@@ -5,7 +5,7 @@ module.exports = [
   {
     name: 'strapi::cors',
     config: {
-      origin: ['http://localhost:3000', 'http://localhost:1337'], // specify allowed origins
+      origin: ['http://localhost:3000', 'http://localhost:1337', 'https://quizai-weld.vercel.app', 'https://quizai-backend.vercel.app'], // specify allowed origins
       methods: ['GET', 'POST', 'PUT', 'DELETE'], // specify allowed methods
       headers: ['Authorization', 'Content-Type', 'bearer'], // specify allowed headers
     },
